33d9a3912ea5aa0e0605077420af2c5f6ed10306
Alexiel Mejias <alexielm@rogers.com>
2019-10-30 14:14:28 -0400

    Initial Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+//import endGame from './data/tic-tac-toe-end_game.json'
+
+function App() {
+  return (
+      <div className="App">
+          RLDatix - Tic-Tac-Toe - Development candidate home work
+    </div>
+  );
+}
+
+export default App;
